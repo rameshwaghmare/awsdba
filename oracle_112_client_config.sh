30b924b258eee92674a0b0fc18202717f03fed51
Ramesh Waghmare <ramesh.waghmare@rackspace.com>
2017-08-19 08:36:10 +1000

    Delete oracle_112_client_config.sh

--- oracle_112_client_config.sh
+++
@@ -1,7 +0,0 @@
-chmod 700 /root/awsdba/oracle_112_get_oracle_repo.sh /root/awsdba/oracle_112_install_client.sh
-/root/awsdba/oracle_112_get_oracle_repo.sh
-/root/awsdba/oracle_112_install_client.sh
-/u01/app/oraInventory/orainstRoot.sh
-cp /root/awsdba/oracle_112_transfer_dump.pl /home/oracle/transfer_dump.pl
-cp /root/awsdba/tnsnames.ora /u01/app/oracle/product/11.2/client_1/network/admin/tnsnames.ora
-chown oracle:oinstall /u01/app/oracle/product/11.2/client_1/network/admin/tnsnames.ora /home/oracle/transfer_dump.pl
