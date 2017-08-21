17f94b29f5ad3ecc3c1853be8824d8fa3d39555b
Ramesh Waghmare <ramesh.waghmare@rackspace.com>
2017-08-21 10:49:04 +1000

    Create 122_oracle_server_config.sh

---
+++ dbserver122/122_oracle_server_config.sh
@@ -0,0 +1,11 @@
+chmod 700 /root/awsdba/dbserver122/122-set_fs_oracle_repo.md 
+chmod 700 /root/awsdba/dbserver122/122-install_oracle_software.md 
+sleep 60
+/u01/app/oraInventory/orainstRoot.sh 
+/u01/app/oracle/product/12.2.0/dbhome_1/root.sh 
+chmod 700 /root/awsdba/dbserver122/122-install_opatch.md 
+chmod 700 /root/awsdba/dbserver122/122-create_oracle_db.md 
+/root/awsdba/dbserver122/122-set_fs_oracle_repo.md 
+/root/awsdba/dbserver122/122-install_oracle_software.md 
+/root/awsdba/dbserver122/122-install_opatch.md 
+/root/awsdba/dbserver122/122-create_oracle_db.md
